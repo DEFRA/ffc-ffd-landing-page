6d134293b9c5913ffc92b00b96b99ffa0a9d29fc
Fay Toward <fay.toward@rpa.gov.uk>
2024-02-02 11:26:35 +0000

    Co-authored-by: Rana Salem <ranatasalem@gmail.com> Co-authored-by: John Watson <johnwatson484@users.noreply.github.com>

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,6 +1,7 @@
 const Wreck = require('@hapi/wreck')
 const { GET } = require('../constants/http-verbs')
 const { USER } = require('ffc-auth/scopes')
+const { serverConfig } = require('../config')
 
 module.exports = [{
   method: GET,
@@ -9,14 +10,26 @@ module.exports = [{
   handler: async (request, h) => {
     const defraIdToken = request.state.ffc_ffd_auth_token
     const crn = request.auth.credentials.crn
-    const { payload } = await Wreck.post('http://ffc-ffd-data:3004', {
+    const query = `query {
+          customerBusinesses {
+            crn
+            businesses {
+              id
+              sbi
+              name
+            }
+          }
+        }`
+    const { payload } = await Wreck.post(serverConfig.dataHost, {
       headers: {
         crn,
-        Authorization: defraIdToken
+        Authorization: defraIdToken,
+        'Content-Type': 'application/json'
       },
+      payload: JSON.stringify({ query }),
       json: true
     })
     console.log(payload)
-    return h.view('home')
+    return h.view('home', { customerBusinesses: payload.data.customerBusinesses })
   }
 }]
